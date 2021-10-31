9b25fe8177a9973b8b132cc8c08e7c2265349d4e
Daniel Falk <daniel@da-robotteknik.se>
2021-10-31 12:50:42 +0100

    Update example app

--- app/app.py
+++ app/app.py
@@ -1,4 +1,11 @@
+"""
+Simple main file to test the cython wrappers for the c functions
+"""
+
 from dummy_data import py_get_numbers
+import numpy as np
 
 if __name__ == "__main__":
-    print("Numbers: %s" % " ".join(str(py_get_numbers(5))))
+    numbers = np.array(py_get_numbers(5))
+    print(numbers)
+    print("Sum of numbers: ", np.sum(numbers))
